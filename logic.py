ced52f224c8579ec1ad9f825e57461a5a3853d4a
andreasofiaramirez-tech <andreasofiaramirez@gmail.com>
2025-11-10 12:08:01 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -838,7 +838,7 @@ def run_conciliation_retenciones(file_cp, file_cg, file_iva, file_islr, file_mun
             'NOMBREPROVEEDOR': ['PROVEEDOR', 'NOMBRE', 'RAZONSOCIAL', 'RAZONSOCIALDELSUJETORETENIDO']
         }
 
-        find_and_rename(df_cp, galac_synonyms['Proveedor'], 'RIF', 'CP')
+        find_and_rename(df_cp, galac_synonyms['RIF'], 'RIF', 'CP')
         find_and_rename(df_cp, galac_synonyms['NOMBREPROVEEDOR'], 'NOMBREPROVEEDOR', 'CP')
         
         for df_galac, nombre_archivo in [(df_galac_iva, 'GALAC IVA'), (df_galac_islr, 'GALAC ISLR'), (df_galac_mun, 'Municipal')]:
